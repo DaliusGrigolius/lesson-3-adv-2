9a43405614406ac3a5d4156364118b70b2a1398c
DaliusGrigolius <dalius.grigolius@gmail.com>
2022-04-27 15:18:29 +0300

    Add project files.

---
+++ lesson 3 adv 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson_3_adv_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ lesson 3 adv 2/SomeGenericClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson_3_adv_2
+{
+    public class SomeGenericClass<T>
+    {
+        private List<T> SomeList { get; set; }
+
+        public SomeGenericClass()
+        {
+            SomeList = new List<T>();
+        }
+
+        public void PrintList()
+        {
+            foreach (var item in SomeList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public T[] RetrieveConvertedListToArray()
+        {
+            return SomeList.ToArray();
+        }
+
+        public void AddItem(T something)
+        {
+            SomeList.Add(something);
+        }
+
+        public void AddListOfItems(List<T> listOfItems)
+        {
+            foreach(var item in listOfItems)
+            {
+                SomeList.Add(item);
+            }
+        }
+
+        public void DeleteItem(T item)
+        {
+            SomeList.Remove(item);
+        }
+
+        public void DeleteItemByIndex(int itemIndex)
+        {
+            SomeList.RemoveAt(itemIndex);
+        }
+
+        public void DeleteAllItems(T item)
+        {
+            for (int i = 0; i < SomeList.Count; i++)
+            {
+                if (SomeList[i].Equals(item))
+                {
+                    SomeList.RemoveAt(i);
+                }
+            }
+        }
+
+        public bool CheckIfValueIsNull(T item)
+        {
+            return item == null ? true : false;
+        }
+    }
+}
